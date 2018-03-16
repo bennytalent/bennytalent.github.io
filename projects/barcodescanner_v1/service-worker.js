47ddbd4aa845a3aac2811f9809338ba7356b0e1b
bennytalent <ben.kagerer@gmx.de>
2018-03-16 12:55:49 +0100

    changed image settings

--- projects/barcodescanner_v1/service-worker.js
+++ projects/barcodescanner_v1/service-worker.js
@@ -13,12 +13,20 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
+
+            filesToCache.forEach(function (file) {
+                cache.add(file).catch(function (reason) {
+
+                });
+            });
+            console.log('[ServiceWorker] Caching failed for file ' + file);
+            //return cache.addAll(filesToCache);
         })
     );
 });
 
-self.addEventListener('activate', function(e) {
+// TODO check new function
+/*self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
     e.waitUntil(
         caches.keys().then(function (keyList) {
@@ -31,9 +39,28 @@ self.addEventListener('activate', function(e) {
         })
     );
     return self.clients.claim();
+});*/
+
+self.addEventListener('activate', function (event) {
+    console.log('[ServiceWorker] Activating new service worker...');
+
+    var chacheWhiteList = [cacheName];
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if(chacheWhiteList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
-// when the browser fetches a URL...
+// when the browser fetches a URL... TODO check new function
+/*
 self.addEventListener('fetch', function (event) {
     // ... either respond with the cached object or go ahead and fetch the actual URL
     event.respondWith(
@@ -44,6 +71,35 @@ self.addEventListener('fetch', function (event) {
             }
             // fetch as normal
             return fetch(event.request);
+        })
+    );
+});*/
+
+self.addEventListener('fetch', function (event) {
+    console.log('[ServiceWorker] Fetch event for ', event.request.url);
+    event.respondWIth(
+        caches.match(event.request).then(function (response) {
+            if(response) {
+                console.log('Found ', event.request.url, ' in cache');
+                return response;
+            }
+            console.log('Network request for ', event.request.url);
+            return fetch(event.request).then(function(response) {
+
+                // TODO 5 - Respond with custom 404 page
+
+                return caches.open(staticCacheName).then(function(cache) {
+                    if (event.request.url.indexOf('test') < 0) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                });
+            });
+
+        }).catch(function (error) {
+
+            // TODO 6 - Respond with custom offline page
+
         })
     );
 });
